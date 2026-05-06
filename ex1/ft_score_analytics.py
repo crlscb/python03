c3b4740304edf2617036975483c688a71864b8da
cescobio <cescobio@student.42madrid.com>
2026-05-06 17:33:43 +0200

    ex1/ft_score_analytics

---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,45 @@
+
+import sys
+
+
+def ft_score_analytics() -> None:
+    print("=== Player Score Analytics ===")
+
+    if len(sys.argv) == 1:
+        print("No scores provided. Usage: "
+              "python3 ft_score_analytics.py <score1> <score2> ...")
+        return
+
+    scores = []
+    for arg in sys.argv[1:]:
+        try:
+            score = int(arg)
+            scores += [score]
+        except ValueError:
+            print(f"Invalid parameter: {arg}")
+
+    if scores:
+        print_score(scores)
+    else:
+        print("No scores provided. Usage: "
+              "python3 ft_score_analytics.py <score1> <score2> ...")
+
+
+def print_score(scores: list[int]) -> None:
+    total_players = len(scores)
+    total_score = sum(scores)
+    average_score = total_score / total_players
+    high_score = max(scores)
+    low_score = min(scores)
+    score_range = high_score - low_score
+
+    print(f"Total players: {total_players}")
+    print(f"Total score: {total_score}")
+    print(f"Averge score: {average_score:.1f}")
+    print(f"High score: {high_score}")
+    print(f"Low score: {low_score}")
+    print(f"Score range: {score_range}")
+
+
+if __name__ == '__main__':
+    ft_score_analytics()
