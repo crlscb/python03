4ef5fdf40b3ad714d6d1550606a648529c6b206c
cescobio <cescobio@student.42madrid.com>
2026-05-07 18:07:56 +0200

    ex5/ft_data_stream

---
+++ ex5/ft_data_stream.py
@@ -0,0 +1,63 @@
+
+import random
+from typing import Generator
+
+PLAYERS = [
+    "bob",
+    "alice",
+    "dylan",
+    "charlie"
+]
+
+ACTIONS = [
+    "run",
+    "eat",
+    "sleep",
+    "grab",
+    "move",
+    "climb",
+    "swim",
+    "use",
+    "release"
+]
+
+
+def gen_event() -> Generator[tuple[str, str], None, None]:
+
+    while True:
+
+        player = random.choice(PLAYERS)
+        action = random.choice(ACTIONS)
+        yield (player, action)
+
+
+def consume_event(events: list[tuple[str, str]]
+                  ) -> Generator[tuple[str, str], None, None]:
+
+    while len(events) > 0:
+
+        i = random.randint(0, len(events) - 1)
+        event = events.pop(i)
+        yield event
+
+
+if __name__ == '__main__':
+    print("=== Game Data Stream Processor ===")
+
+    events = gen_event()
+
+    for i in range(1000):
+
+        player, action = next(events)
+        print(f"Event {i}: Player {player} did action {action}")
+
+    event_list = []
+
+    for i in range(10):
+        event_list.append(next(events))
+    print(f"Built listo of 10 events: {event_list}")
+
+    for event in consume_event(event_list):
+
+        print(f"Got event from list: {event}")
+        print(f"Remains in list: {event_list}")
